5a3de34dc08140ece74c2cec0300a6c4e1afa1c7
lukas.haken <lukyh735@gmail.com>
2021-09-21 14:38:08 +0200

    Zelená je tráva, fotbalová sláva

--- src/Package/Main.java
+++ src/Package/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Ahoj světe");
+        System.out.println("Zelená je tráva, fotbalová sláva");
     }
 }
